baee94b562204fe2795f519aefecba5c7000759e
Rustam4457 <punk60826@gmail.com>
2018-06-13 03:13:59 +0300

    Add firebase apo keys and material design

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCGPySPyFVGN_Mpjg9taZ1n3oCNAbDnp9E",
+    authDomain: "ng-fire-a0a9f.firebaseapp.com",
+    databaseURL: "https://ng-fire-a0a9f.firebaseio.com",
+    projectId: "ng-fire-a0a9f",
+    storageBucket: "",
+    messagingSenderId: "362544246535"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
