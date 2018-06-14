76bc1b6d081917b01a632f88ea2a45863a9902e8
Rustam4457 <punk60826@gmail.com>
2018-06-14 21:28:03 +0300

    angularfire2 install and generate module and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AngularFireModule} from 'angularfire2';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './/material.module';
 import { CoreModule } from './core/core.module';
+import { RoutingModule } from './/routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     MaterialModule,
-    CoreModule
+    CoreModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AuthModule } from '../auth/auth.module';
 
 @NgModule({
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AuthModule
   ],
   declarations: []
 })
